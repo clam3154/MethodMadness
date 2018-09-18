f7f2d58f918639caf025e2625512302d7bf2f549
clam3154 <clam3154@bths.edu>
2018-09-18 10:14:29 -0400

    Recent

--- src/CLam/CLamLib.java
+++ src/CLam/CLamLib.java
@@ -6,4 +6,18 @@ public class CLamLib
     {
         System.out.println(str);
     }
+    public static String dateStr(String day)
+    {
+        String date = day.substring(3,5) + "-" + day.substring(0,2) + "-" + day.substring(6);
+        System.out.println(date);
+        return date;
+    }
+    public static String cutOut(String mainStr, String subStr)
+    {
+        String word = mainStr.substring(0, mainStr.indexOf(subStr));
+        word += mainStr.substring(subStr.length() + mainStr.indexOf(subStr));
+        System.out.println(word);
+        return word;
+    }
+    public static
 }
--- src/CLam/TestFile.java
+++ src/CLam/TestFile.java
@@ -1,11 +1,15 @@
 package CLam;
 
+import static CLam.CLamLib.cutOut;
 import static CLam.CLamLib.println;
+import static CLam.CLamLib.dateStr;
 
 public class TestFile
 {
     public static void main(String[] args)
     {
         println("I love a cup of Java in the morning.");
+        dateStr("mm/dd/yyyy");
+        cutOut("supercomputerstar","computer");
     }
 }
