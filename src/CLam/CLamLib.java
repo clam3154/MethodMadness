693fbfac523fdc0383bc6b6fcc0cfa0258cd7b93
BT_1N3_22@bktech.local <BT_1N3_22@bktech.local>
2018-09-18 09:19:54 -0400

    Initial commit

---
+++ src/CLam/CLamLib.java
@@ -0,0 +1,9 @@
+package CLam;
+
+public class CLamLib
+{
+    public static void println(String str)
+    {
+        System.out.println(str);
+    }
+}
---
+++ src/CLam/TestFile.java
@@ -0,0 +1,11 @@
+package CLam;
+
+import static CLam.CLamLib.println;
+
+public class TestFile
+{
+    public static void main(String[] args)
+    {
+        println("I love a cup of Java in the morning.");
+    }
+}
